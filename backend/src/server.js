2a987776cf31f1fabeec5779fc1757dcf4d4eb88
DeWebAlpha <tribillio@gmail.com>
2026-08-18 09:24:27 +0000

    wire up database connection before starting the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,19 @@
 import { app } from "./app.js";
+import { connectDatabase } from "./config/database.js";
+import { config } from "./config/index.js";
 
-const PORT = 4000;
 
-const server = app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
-})
+const startServer = async() => {
+    try{
+        await connectDatabase();
+
+        const server = app.listen(config.port, () => {
+            console.log(`Listening on port: ${config.port}`);
+        })
+    }catch(error){
+        console.error({err: error}, "Server connection error");
+        process.exit(1);
+    }
+}
+
+startServer()
