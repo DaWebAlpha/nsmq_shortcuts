1c7ad2680ef1e650cedd7d14b45fda07001da734
DeWebAlpha <tribillio@gmail.com>
2026-08-18 15:27:51 +0000

    add middlewares barrel

---
+++ backend/src/middlewares/index.js
@@ -0,0 +1,10 @@
+/**
+ * Barrel file re-exporting every shared middleware.
+ */
+export {
+    notFound
+} from "./notFound.middleware.js";
+
+export {
+    errorHandler
+} from "./errorHandler.middleware.js";
