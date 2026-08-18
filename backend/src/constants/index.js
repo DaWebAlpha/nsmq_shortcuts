d979cb954078dd91782d88d5ade1bb90b0deb512
DeWebAlpha <tribillio@gmail.com>
2026-08-18 11:05:28 +0000

    document constants barrel file

--- backend/src/constants/index.js
+++ backend/src/constants/index.js
@@ -1,1 +1,8 @@
+/**
+ * Barrel file re-exporting every constant module.
+ */
 export { MONGOOSE_OPTIONS } from "./mongoose.options.js";
+export { SENSITIVE_FIELDS } from "./sensitiveFields.js";
+export { HTTP_STATUS } from "./httpStatus.js";
+
+
